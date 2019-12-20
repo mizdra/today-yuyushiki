2dd565f2bd3ec4db24f96a1237dbac984213b042
mizdra <pp.mizdra@gmail.com>
2019-12-21 01:15:13 +0900

    リファクタリング

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,14 @@
 import { promises as fs } from 'fs';
 import EscPosEncoder from 'esc-pos-encoder';
 
+// 区間 [0, max) の中の整数をランダムで返す
 function getRandomInt(max: number) {
   return Math.floor(Math.random() * Math.floor(max));
 }
 
-(async () => {
-  if (process.env.KOMA_DIR === undefined) {
-    throw new Error('環境変数 `KOMA_DIR` を指定して下さい.');
-  }
-
-  const fileNames = await fs.readdir(process.env.KOMA_DIR);
+// ランダムで1コマ選んでそのパスを返す
+async function randomKomaPath(komaDir: string) {
+  const fileNames = await fs.readdir(komaDir);
 
   // .gitkeep や pad-shaved なコマ画像を除外
   const filteredFileNames = fileNames.filter((fileName) =>
@@ -18,8 +16,16 @@ function getRandomInt(max: number) {
   );
 
   // ランダムで1コマ選ぶ
-  const targetFileName =
-    filteredFileNames[getRandomInt(filteredFileNames.length)];
+  return filteredFileNames[getRandomInt(filteredFileNames.length)];
+}
 
-  console.log(targetFileName);
+(async () => {
+  if (process.env.KOMA_DIR === undefined) {
+    throw new Error('環境変数 `KOMA_DIR` を指定して下さい.');
+  }
+
+  // ランダムで1コマ選ぶ
+  const komaPath = await randomKomaPath(process.env.KOMA_DIR);
+
+  console.log(komaPath);
 })();
