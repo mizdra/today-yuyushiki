586619e882a4380c441c71fd8d4d7548e20c670e
mizdra <pp.mizdra@gmail.com>
2019-12-21 01:06:15 +0900

    ランダムでコマを取得するロジックを実装

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,24 @@
-console.log('hello world');
-const a = 1;
+import { promises as fs } from 'fs';
+
+function getRandomInt(max: number) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+(async () => {
+  if (process.env.KOMA_DIR === undefined) {
+    throw new Error('環境変数 `KOMA_DIR` を指定して下さい.');
+  }
+
+  const fileNames = await fs.readdir(process.env.KOMA_DIR);
+
+  // .gitkeep や pad-shaved なコマ画像を除外
+  const filteredFileNames = fileNames.filter((fileName) =>
+    /\d\d-\d\d\d-\d.jpg/.test(fileName),
+  );
+
+  // ランダムで1コマ選ぶ
+  const targetFileName =
+    filteredFileNames[getRandomInt(filteredFileNames.length)];
+
+  console.log(targetFileName);
+})();
