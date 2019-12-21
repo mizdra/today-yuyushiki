baf968a30e8126b45062007b2f2798ac67ac5962
mizdra <pp.mizdra@gmail.com>
2019-12-21 22:23:47 +0900

    左に寄ってるのが気になるので `ESC / ` 復活させる

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ const 白黒反転を有効化 = [0x1d, 0x42, 1]; // (GS B)
 const 白黒反転を解除 = [0x1d, 0x42, 0]; // (GS B)
 const 文字サイズを4倍に = [0x1d, 0x21, 0b00110011]; // (GS !)
 const 文字サイズを1倍に = [0x1d, 0x21, 0b00000000]; // (GS !)
+const 右に少しずらす = [0x1b, 0x5c, 30, 0]; // (ESC \)
 
 (async () => {
   if (process.env.KOMA_DIR === undefined) {
@@ -30,14 +31,14 @@ const 文字サイズを1倍に = [0x1d, 0x21, 0b00000000]; // (GS !)
     .raw(漢字のサイズ変更を有効化)
     .raw(白黒反転を有効化)
     .raw(文字サイズを4倍に)
-    .align('center').jtext('　　今日のゆゆ式　　').newline()
+    .align('center').raw(右に少しずらす).jtext('　　今日のゆゆ式　　').newline()
     .raw(文字サイズを1倍に)
     .raw(白黒反転を解除)
     .newline()
     // コマ画像
-    .align('center').image(Canvas, koma.img, koma.width, koma.height, 'atkinson').newline()
+    .align('center').raw(右に少しずらす).image(Canvas, koma.img, koma.width, koma.height, 'atkinson').newline()
     // コマの説明
-    .align('center').jtext(koma.description).newline()
+    .align('center').raw(右に少しずらす).jtext(koma.description).newline()
     .newline()
     .newline()
     .newline()
